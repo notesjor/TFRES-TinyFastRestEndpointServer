2eb033df712b9d3ae71967f4d348d8b47f76e18c
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2018-12-11 10:20:20 +0100

    // Add Documentation/...

---
+++ TefresWebserver/Documentation/SericeDocumentation.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Tfres.Documentation
+{
+  public class SericeDocumentation
+  {
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("description")]
+    public string Description { get; set; }
+
+    [JsonProperty("endpoints")]
+    public ServiceEndpoint[] Endpoints { get; set; }
+  }
+}
---
+++ TefresWebserver/Documentation/ServiceArgument.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Tfres.Documentation
+{
+  public class ServiceArgument : ServiceParameter
+  {
+    [JsonProperty("isRequired")]
+    public bool IsRequired { get; set; }
+  }
+}
