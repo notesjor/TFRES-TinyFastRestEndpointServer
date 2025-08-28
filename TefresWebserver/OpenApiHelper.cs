3340b9e0cce687d117941068f1261d2da8d61e67
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2025-08-28 14:04:54 +0200

    // nuget

--- TefresWebserver/OpenApiHelper.cs
+++ TefresWebserver/OpenApiHelper.cs
@@ -2,6 +2,7 @@
 
 using System.Globalization;
 using System.IO;
+using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Writers;
 
@@ -11,12 +12,13 @@ namespace Tfres
 {
   public static class OpenApiHelper
   {
-    public static string ConvertToJson(this OpenApiDocument document)
+    public static string ConvertToJson(OpenApiDocument document)
     {
       var outputStringWriter = new StringWriter(CultureInfo.InvariantCulture);
       var writer = new OpenApiJsonWriter(outputStringWriter);
       document.SerializeAsV3(writer);
       writer.Flush();
+      //writer.FlushAsync().Wait(); -> ab 2.0
       return outputStringWriter.GetStringBuilder().ToString();
     }
   }
